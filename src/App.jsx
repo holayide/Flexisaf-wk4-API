6afb700cbbc26cc5448e2b57a421ec6dea50ff98
olaide <holaryidealabi@gmail.comgit config --global user.email holaryidealabi@gmail.com>
2024-10-08 10:34:02 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [advice, setAdvice] = useState("");
+
+  async function getQuote() {
+    const resp = await fetch("https://api.adviceslip.com/advice");
+    const data = await resp.json();
+    setAdvice(data.slip.advice);
+  }
+
+  useEffect(() => {
+    getQuote();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Random Advice Generator</h1>
+      <div className="box">
+        <h3>&quot; {advice} &quot;</h3>
+
+        <div>
+          <button onClick={getQuote} className="btn">
+            Generate
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
